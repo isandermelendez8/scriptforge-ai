3bdf3981d1a2aa1af94cd046bc432566a25b3970
isandermelendez8 <isandermelendez8@gmail.com>
2026-03-10 16:05:15 -0400

    Update me.js

--- api/auth/me.js
+++ api/auth/me.js
@@ -1,18 +1,1 @@
-// api/auth/me.js
-// Returns the current logged-in user from session cookie
 
-export default function handler(req, res) {
-  const cookie = req.headers.cookie || '';
-  const match = cookie.match(/sf_session=([^;]+)/);
-
-  if (!match) {
-    return res.status(200).json({ user: null });
-  }
-
-  try {
-    const sessionData = JSON.parse(Buffer.from(match[1], 'base64').toString('utf8'));
-    return res.status(200).json({ user: sessionData });
-  } catch (e) {
-    return res.status(200).json({ user: null });
-  }
-}
